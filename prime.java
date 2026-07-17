4fee308baea0784b39408d03973f6263efbb09b3
kumuthini <kumutharamasamy75@gmail.com>
2026-07-17 15:12:57 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=17;
+		String str=prime(n);
+		System.out.println(str);
+	}
+	static String prime(int k)
+	{
+	    if(k<2)
+	    {
+	        return"not prime";
+	}
+	for(int i=2;i<k;i++)
+	{
+	    if(k%i==0)
+	    return"not prime";
+	}
+	return"prime";
+}
+}
