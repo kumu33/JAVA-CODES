c6b962faaf0564b3b577425ba9a3bb317c53a83c
kumuthini <kumutharamasamy75@gmail.com>
2026-07-17 15:39:15 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=5;
+		int fact=factorial(1,n);
+		System.out.println("factorial="+fact);
+	}
+	static int factorial(int k,int y)
+	{
+	    if(y==1){
+	        return k;
+	    }
+	    k=k*y;
+	    return factorial(k,y-1);
+	}
+}
