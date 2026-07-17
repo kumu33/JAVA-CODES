adfeeec15006857b9db1cb12867dff6cecc9fa71
kumuthini <kumutharamasamy75@gmail.com>
2026-07-17 14:55:41 +0530

    Create patterrnthree.java

---
+++ patterrnthree.java
@@ -0,0 +1,23 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    int m=10;
+	    pat(n);
+	    pat(m);
+	    pat(3);
+	    pat(4);
+	}
+	static void pat(int k)
+	{
+	    for(int i=0;i<k;i++)
+	    
+{
+    for(int j=i;j<k;j++)
+    {
+        System.out.print(j);
+    }
+    System.out.println();
+}
+	}
+}
