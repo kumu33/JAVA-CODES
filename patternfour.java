6dbf25c9df42f85e9d1d7b29bc4dd1ec5245a270
kumuthini <kumutharamasamy75@gmail.com>
2026-07-20 15:32:21 +0530

    Create patternfour.java

---
+++ patternfour.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i=0;i<4;i++)
+		{
+		    for(int j=0;j<i;j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    for(int j=0;j<4-i;j++)
+		    {
+		        System.out.print(j+1);
+		    }
+		    System.out.println();
+		}
+	}
+}
