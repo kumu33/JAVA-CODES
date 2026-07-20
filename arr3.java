22752a15d6bb03c39b4033aceb0b574f9b103ca2
kumuthini <kumutharamasamy75@gmail.com>
2026-07-20 14:59:41 +0530

    Create arr3.java

---
+++ arr3.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int[] marks={483,454,465,465,467,434,423,421,453,431};
+		String[] names={"abi","aravind","ajay","priya","ramya","hari","tamil","kavitha","kalai","arun"};
+for(int i=0;i<names.length;i++)
+{
+    System.out.println(names[i]+"="+marks[i]);
+}
+}
+}
