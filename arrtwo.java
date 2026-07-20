7f759414e8131522fd82fd914a8452ab17e663ca
kumuthini <kumutharamasamy75@gmail.com>
2026-07-20 14:49:10 +0530

    Create arrtwo.java

---
+++ arrtwo.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	int[] marks={98,97,98,99,89};
+	String[] subject={"tamil","english","maths","science","social"};
+	for(int i=0;i<subject.length;i++)
+	{
+	    System.out.println(subject[i]+"="+marks[i]);
+	}
+}
+}
