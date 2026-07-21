37c3d2adc469358d390fef0790aac7799702b103
kumuthini <kumutharamasamy75@gmail.com>
2026-07-21 15:06:42 +0530

    Create matrixtransporse.java

---
+++ matrixtransporse.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int [] [] arr={
+		    {1,2,3},
+		    {4,5,6}
+		};
+		System.out.println("Transporse matrix:");
+		for(int i=0;i<arr[0].length;i++)
+		{
+		    for(int j=0;j<arr.length;j++)
+		    {
+		        System.out.print(arr[j][i]+" ");
+		    }
+		    System.out.println();
+		    }
+	}
+}
