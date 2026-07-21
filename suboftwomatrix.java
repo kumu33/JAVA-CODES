7ccd85e49e4d102ba8b531478c8899c27b009ab2
kumuthini <kumutharamasamy75@gmail.com>
2026-07-21 15:00:52 +0530

    Create suboftwomatrix.java

---
+++ suboftwomatrix.java
@@ -0,0 +1,24 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int[] []a={{1,2,3},{4,5,6},{7,8,9}};
+		int[] []b={{4,6,7},{8,9,10},{11,12,13}};
+		int[] []c=new int[3][3];
+		for(int i=0;i<a.length;i++)
+		{
+		    for(int j=0;j<a[0].length;j++)
+		
+		{
+		    c[i][j]=b[i][j]-a[i][j];
+		}
+	}
+	for(int i=0;i<a.length;i++)
+	{
+	    for(int j=0;j<a[0].length;j++)
+	    {
+	        System.out.print(c[i][j]+" ");
+	    }
+	    System.out.println();
+	}
+}
+}
