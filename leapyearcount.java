063a66e48c8267fe7e74007095a53edc0619f742
kumuthini <kumutharamasamy75@gmail.com>
2026-07-30 16:50:00 +0530

    Create leapyearcount.java

---
+++ leapyearcount.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	     int count =0;
+		for(int i=2005;i<=2026;i++){
+		    if(i%4==0){
+		        count++;
+		    }
+		       
+		}
+		System.out.println("total leap year="+count);
+}
+}
