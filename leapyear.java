a368a6b0c279a0139b59e961d216974f137060af
kumuthini <kumutharamasamy75@gmail.com>
2026-07-30 16:48:10 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i=2005;i<=2026;i++){
+		    if(i%4==0){
+		        System.out.println("leap year");
+		    }
+		        
+		            else{
+		                System.out.println(i);
+		            }
+		    }
+		}
+}
